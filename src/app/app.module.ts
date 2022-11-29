b777afa9c63bb2fb60972fef18eef9faeb353dbd
Nicolas Temprano <nicomxd22@gmail.com>
2022-11-29 19:15:20 -0300

    desafio lista de alumnos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { StudentsComponent } from './students/students.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { ConnectionsComponent } from './connections/connections.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    StudentsComponent,
+    ToolbarComponent,
+    ConnectionsComponent
   ],
   imports: [
     BrowserModule
